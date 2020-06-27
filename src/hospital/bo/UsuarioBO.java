23183c3bc60d00f5560e509fa639450ec63a9334
Junior <junior_5u@hotmail.com>
2020-06-27 11:44:39 -0500

    Login terminado

--- src/hospital/bo/UsuarioBO.java
+++ src/hospital/bo/UsuarioBO.java
@@ -5,10 +5,35 @@
  */
 package hospital.bo;
 
+import hospital.dao.UsuarioDAO;
+import hospital.db.Conexion;
+import java.sql.Connection;
+import java.sql.SQLException;
+
 /**
  *
  * @author PC
  */
 public class UsuarioBO {
+    private String mensaje = "";
+    private UsuarioDAO udao = new UsuarioDAO();
     
+    public String login(String p, String user){
+        Connection conn = Conexion.getConnection();
+        String pass = udao.login(conn,user);
+        try {
+            if(pass == null){
+                mensaje = "Usuario incorrecto";
+            }
+            else if(p.equals(pass)){
+                mensaje = "Contraseña correcta";
+            }else{
+                mensaje = "Contraseña incorrecta";
+            }
+            conn.close();
+        } catch (SQLException ex) {
+            System.out.println(ex.getMessage());      
+        }
+        return mensaje;
+    }
 }
--- src/hospital/dao/UsuarioDAO.java
+++ src/hospital/dao/UsuarioDAO.java
@@ -21,8 +21,7 @@ public class UsuarioDAO {
     
     private String mensaje = "";
     
-    public String login(Connection con){
-        String user = "juniorosis20@gmail.com";
+    public String login(Connection con, String user){
         String pass = null;
         PreparedStatement pst = null;
         ResultSet rs = null;
@@ -33,7 +32,6 @@ public class UsuarioDAO {
             rs = pst.executeQuery();
             if(rs.next()){
                 pass = rs.getString("US_CLAVE");
-                System.out.println(pass);
             }
             rs.close();
             pst.close();
@@ -41,28 +39,5 @@ public class UsuarioDAO {
             System.out.println("Error en usuario DAO: " + e.getMessage());
         }
         return pass;
-    }
-    
-    public static void main(String[] args){
-        UsuarioDAO user = new UsuarioDAO();
-        
-        Connection conn = null;
-        String login = "Proyecto";
-        String clave = "123456";
-        String url = "jdbc:oracle:thin:@localhost:1521:xe";
-        
-        try {
-            Class.forName("oracle.jdbc.driver.OracleDriver");
-            conn = DriverManager.getConnection(url,login,clave);
-            conn.setAutoCommit(false);
-            if(conn != null){
-                System.out.println("Conexión Exitosas");
-            }else{
-                System.out.println("Conexion es erronea");
-            }
-        } catch (ClassNotFoundException | SQLException e) {
-            JOptionPane.showMessageDialog(null, "Conexión Erronea " + e.getMessage());
-        }
-        user.login(conn);
     }
 }
