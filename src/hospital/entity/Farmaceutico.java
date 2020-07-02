666b7fcad58cf79fa0f1cd0dfe1fcf649bb1b655
Andre2911 <joniguevara1@gmail.com>
2020-07-02 15:42:09 -0500

    Entity farmacia

---
+++ src/hospital/entity/Farmaceutico.java
@@ -0,0 +1,87 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package hospital.entity;
+
+/**
+ *
+ * @author PROPIETARIO
+ */
+public class Farmaceutico extends Usuario {
+    private int idfarmaceutico;
+    private int dni;
+    private String nombres;
+    private String apellidos;
+    private char genero;
+    private int edad;
+
+    public Farmaceutico(){
+        
+    }
+
+    public Farmaceutico(int idfarmaceutico, int dni, String nombres, String apellidos, char genero, int edad) {
+        this.idfarmaceutico = idfarmaceutico;
+        this.dni = dni;
+        this.nombres = nombres;
+        this.apellidos = apellidos;
+        this.genero = genero;
+        this.edad = edad;
+    }
+
+    public int getIdfarmaceutico() {
+        return idfarmaceutico;
+    }
+
+    public void setIdfarmaceutico(int idfarmaceutico) {
+        this.idfarmaceutico = idfarmaceutico;
+    }
+
+    public int getDni() {
+        return dni;
+    }
+
+    public void setDni(int dni) {
+        this.dni = dni;
+    }
+
+    public String getNombres() {
+        return nombres;
+    }
+
+    public void setNombres(String nombres) {
+        this.nombres = nombres;
+    }
+
+    public String getApellidos() {
+        return apellidos;
+    }
+
+    public void setApellidos(String apellidos) {
+        this.apellidos = apellidos;
+    }
+
+    public char getGenero() {
+        return genero;
+    }
+
+    public void setGenero(char genero) {
+        this.genero = genero;
+    }
+
+    public int getEdad() {
+        return edad;
+    }
+
+    public void setEdad(int edad) {
+        this.edad = edad;
+    }
+    
+    
+    
+    
+    
+    
+    
+}
