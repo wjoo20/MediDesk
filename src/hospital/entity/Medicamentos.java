66dbd1a499d3f96464eb451d393e1d19fa05deaa
Andre2911 <joniguevara1@gmail.com>
2020-07-02 16:14:57 -0500

    conflictos

--- src/hospital/entity/Medicamentos.java
+++ src/hospital/entity/Medicamentos.java
@@ -11,4 +11,43 @@ package hospital.entity;
  */
 public class Medicamentos {
     
+    private int idmedicamento;
+    private String nombre;
+    private int cantidad;
+
+public Medicamentos(){
+    
 }
+
+    public Medicamentos(int idmedicamento, String nombre, int cantidad) {
+        this.idmedicamento = idmedicamento;
+        this.nombre = nombre;
+        this.cantidad = cantidad;
+    }
+
+    public int getIdmedicamento() {
+        return idmedicamento;
+    }
+
+    public void setIdmedicamento(int idmedicamento) {
+        this.idmedicamento = idmedicamento;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public int getCantidad() {
+        return cantidad;
+    }
+
+    public void setCantidad(int cantidad) {
+        this.cantidad = cantidad;
+    }
+
+}
+
